7cf3e53ea4b5b5ca45e1c1b36f307fa86aa353e7
Wangzihao5325 <416990273@qq.com>
2019-07-24 17:13:17 +0700

    1.lazy import

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
+import { Suspense, lazy } from 'react';
 import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import { connect } from 'react-redux';
-
-import Main from '../screen/main/index';
 import InitialComponent from './initialComponent';
+
+const Main = lazy(() => import('../screen/main/index'));
 
 const AppRouter = (props: any) => {
     if (props.isInitial) {
         return (
-            <Router>
-                <Switch>
-                    <Route path='/' exact component={Main} />
-                </Switch>
-            </Router>
+            <Suspense fallback={<div>加载ing!</div>}>
+                <Router>
+                    <Switch>
+                        <Route path='/' exact component={Main} />
+                    </Switch>
+                </Router>
+            </Suspense>
         );
     } else {
         return (
